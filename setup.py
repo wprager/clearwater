1105811cb69ace16f0034e8117afcbfca3fe4ea1
wprager <wesprager@gmail.com>
2015-10-28 14:28:56 -0700

    updated setup to match readme

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from clearwater import db
-from clearwater.config import SQLALCHEMY_DATABASE_URI
+# from clearwater.config import SQLALCHEMY_DATABASE_URI
 from clearwater.controllers import hash_pass
 from clearwater.models import Base, User, Measurement
-from sqlalchemy import create_engine
+# from sqlalchemy import create_engine
 
-engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
-Base.metadata.create_all(engine)
+# engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
+# Base.metadata.create_all(engine)
 admin = User('admin', hash_pass('admin'), 1)
 db.session.add(admin)
 db.session.commit()
